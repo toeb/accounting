1f4e26bfd56aa35787f2367b2f878a714fdf4786
Tobias Becker <becker.tobi@gmail.com>
2014-11-13 23:33:48 +0100

    added webapi2 and owin startup class #12

---
+++ Accounting/Application.Web/Startup.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.Owin;
+using Owin;
+using System.Web.Http;
+
+[assembly: OwinStartup(typeof(Application.Web.Startup))]
+
+namespace Application.Web
+{
+  public class Startup
+  {
+    public void Configuration(IAppBuilder app)
+    {
+      // Configure Web API for self-host. 
+      HttpConfiguration config = new HttpConfiguration();
+      config.Routes.MapHttpRoute(
+          name: "DefaultApi",
+          routeTemplate: "api/{controller}/{id}",
+          defaults: new { id = RouteParameter.Optional }
+      );
+
+      app.UseWebApi(config); 
+    }
+  }
+}
