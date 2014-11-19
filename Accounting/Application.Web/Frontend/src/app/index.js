0810e4c8c847a7b723f9e402f23cff2d17c2fedf
Tobias Becker <becker.tobi@gmail.com>
2014-11-19 15:29:46 +0100

    worked on a sesible navigation for accounts

--- Accounting/Application.Web/Frontend/src/app/index.js
+++ Accounting/Application.Web/Frontend/src/app/index.js
@@ -3,21 +3,68 @@
 angular.module('frontend', ['ngResource', 'ui.router', 'ngMaterial'])
   .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider
-      .state('home', {
+      .state('app', {
         url: '/',
-        templateUrl: 'app/views/main/main.html',
-        controller: 'MainCtrl'
+        views: {
+          "": {
+            templateUrl: 'app/views/main/main.html',
+            controller: 'MainCtrl'
+          },
+          "navigation": {
+            templateUrl: 'components/navbar/navbar.html',
+            controller: 'NavbarCtrl'
+          }
+        }
       })
-    .state('api', {
-      url: '/api',
-      templateUrl: 'app/views/api/api.html',
-      controller:'ApiCtrl'
-    })
-    .state('accounts',{
+    .state('accounts', {
       url: '/accounts',
-      templateUrl: 'app/views/accounts/accounts.html',
-      controller:'AccountsCtrl'
+      abstract:true,
+      views: {
+        "": {
+          templateUrl: 'app/views/accounts/accounts.html',
+          controller: 'AccountsCtrl'
+        },
+        "navigation": {
+          templateUrl: 'components/navbar/navbar.html',
+          controller: 'NavbarCtrl'
+        }
+      },
+      resolve: {
+        accounts: ['accountingService', function (accountingService) {
+          return accountingService.getAccounts();
+        }]
+      }
+    })
+    .state('accounts.open', {
+      url: '/open',
+      displayName:'Open Account',
+      views: {
+        "": {
+          templateUrl: 'app/views/accounts/open.html',
+          controller: 'AccountsCtrl'
+        }
+      }
+    })
+    .state('accounts.list', {
+      displayName:'Show Accounts',
+      url: '',
+      views: {
+        "": {
+          templateUrl: 'app/views/accounts/list.html',
+          controller:'AccountsCtrl'
+        }
+      }
+    })
+    .state('accounts.details', {
+      url: '/{accountId:[0-9]{1,4}}',
+      views:{
+        '':{
+          templateUrl: 'app/views/accounts/details.html',
+          controller:'AccountsCtrl'
+        }
+      }
     });
+
 
     $urlRouterProvider.otherwise('/');
   })
--- Accounting/Application.Web/Frontend/src/components/navbar/navbar.controller.js
+++ Accounting/Application.Web/Frontend/src/components/navbar/navbar.controller.js
@@ -1,6 +1,17 @@
 'use strict';
 
 angular.module('frontend')
-  .controller('NavbarCtrl', function ($scope) {
-    $scope.date = new Date();
+  .controller('NavbarCtrl', function ($scope, $state) {
+    $scope.selectedItem = {};
+
+    var states = $state.get();
+    console.log(states);
+    $scope.items = [];
+    states.forEach(function (state) {
+      console.log(state);
+      if (!!state.name && !state.abstract &&!!state.displayName) {
+        $scope.items.push(state);
+      }
+    });
+
   });
