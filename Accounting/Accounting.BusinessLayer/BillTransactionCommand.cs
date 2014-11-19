a5f0fa949791ba0b8e6ca840b133dea7b8adea54
Tobias Becker <becker.tobi@gmail.com>
2014-11-19 02:45:06 +0100

    #3 added BillTransaction command handler to accounting facade, checks are performed on billtransaction command wether it is valid or not and if so it is created

---
+++ Accounting/Accounting.BusinessLayer/BillTransactionCommand.cs
@@ -0,0 +1,26 @@
+﻿using Accounting.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace Accounting.BusinessLayer
+{
+  public class BillTransactionCommand
+  {
+    public BillTransactionCommand() { PartialTransactions = new List<PartialTransaction>(); }
+    public string Receipt { get; set; }
+    public DateTime? ReceiptDate { get; set; }
+    public string TransactionText { get; set; }
+
+    /// <summary>
+    ///  partial transactions to perform
+    /// </summary>
+    public IList<PartialTransaction> PartialTransactions { get; set; }
+
+    /// <summary>
+    /// output transaction 
+    /// </summary>
+    public Transaction Transaction { get; set; }
+  }
+}
