5d12ec5c560dad4776939eedc1b014e97efdb9f1
matthiaszoellner <matthias_zoellner@arcor.de>
2014-11-29 16:51:30 +0100

    Providing the UpdateAccountCommand functionality (see #1)

--- Accounting/Accounting.BusinessLayer/Commands/UpdateAccountCommand.cs
+++ Accounting/Accounting.BusinessLayer/Commands/UpdateAccountCommand.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using Accounting.Model;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -7,6 +8,29 @@ namespace Accounting.BusinessLayer
 {
   public class UpdateAccountCommand
   {
+    /// <summary>
+    /// Id of the account, where an update is requested
+    /// </summary>
+    public int AccountId { get; set; }
 
+    /// <summary>
+    /// Optional new value for the account number. The number is required to be unique across all accounts
+    /// </summary>
+    public string NewNumber { get; set; }
+
+    /// <summary>
+    /// Optional new value for the account display shortname. The shortname is required to be unique across all accounts
+    /// </summary>
+    public string NewShortName { get; set; }
+
+    /// <summary>
+    /// Optional new value for the account display name. The name is required to be unique across all accounts
+    /// </summary>
+    public string NewName { get; set; }
+
+    /// <summary>
+    /// This field is set by the AccountingFacade after successful update of specified fields
+    /// </summary>
+    public Account ModifiedAccount { get; set; }
   }
 }
